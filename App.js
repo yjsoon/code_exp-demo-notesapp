43add4909762d467b8791c26203f31bb56478984
YJ Soon <yjsoon@tinkertanker.com>
2021-06-10 15:35:41 +0800

    Added stack to home screen and styled header

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+function NotesScreen() {
+  return <View style={styles.container}></View>;
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            headerTitle: "Notes App",
+            headerTitleStyle: styles.headerTitleStyle,
+            headerStyle: styles.headerStyle,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#cdd",
+  },
+  headerTitleStyle: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  headerStyle: {
+    height: 80,
+    backgroundColor: "#aa9",
+    borderBottomWidth: 3,
+    borderBottomColor: "green",
   },
 });
